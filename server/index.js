f188226b1209f154e15acda638bc2703ee991b78
hhoonjjang <111038171+hhoonjjang@users.noreply.github.com>
2023-03-20 15:18:16 +0900

    0320 기본틀

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const session = require("express-session");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const cors = require("cors");
+
+const routes = require("./routes");
+const db = require("./models");
+
+dotenv.config();
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cors({ origin: true, credentials: true }));
+app.use("/", express.static(path.join(__dirname, "build")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: "seed",
+  })
+);
+app.use("/api", routes);
+
+db.sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.listen(8080, () => {
+  console.log(8080 + " server start");
+});
