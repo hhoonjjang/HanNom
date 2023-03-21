214275fca273ba6510d351791267b3c4221fd474
hhoonjjang <111038171+hhoonjjang@users.noreply.github.com>
2023-03-21 11:11:00 +0900

    0321

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 const cors = require("cors");
 
-const routes = require("./routes");
-const db = require("./models");
+// const routes = require("./routes");
+// const db = require("./models");
 
 dotenv.config();
 
@@ -31,16 +31,16 @@ app.use(
     name: "seed",
   })
 );
-app.use("/api", routes);
+// app.use("/api", routes);
 
-db.sequelize
-  .sync({ force: false })
-  .then(() => {
-    console.log("db connected");
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+// db.sequelize
+//   .sync({ force: false })
+//   .then(() => {
+//     console.log("db connected");
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
 
 app.listen(8080, () => {
   console.log(8080 + " server start");
