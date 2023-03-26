6ba7cae6f5d46306b6a56a824920211493cb17e6
YoungjunKim <76671521+color99b@users.noreply.github.com>
2023-03-26 16:30:47 +0900

    Update Component.jsx

--- front/src/components/Home/Component.jsx
+++ front/src/components/Home/Component.jsx
@@ -5,10 +5,51 @@ import {
   UserInfoCom3,
   YellowButton,
 } from "../../ui-components";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const HomeComponent = ({}) => {
+  const [user, setUser] = useState();
+  const info = async () => {
+    const [_account] = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    console.log("_account : ", _account);
+    const userInfo = (
+      await axios.post("http://localhost:8080/api/user/login", {
+        account: _account,
+      })
+    ).data;
+    setUser(userInfo);
+    console.log(user);
+  };
+  useEffect(() => {
+    info();
+  }, []);
+
   return (
     <Home>
+      <button
+        onClick={() => {
+          info();
+        }}
+      >
+        test
+      </button>
+      <InfoTestComp>
+        {user ? (
+          <div>
+            <div>userAddress :{user.userAddress}</div>
+            <div>userName :{user.userName}</div>{" "}
+            <div>
+              profileImg:
+              <img src={"http://localhost:8080" + user.profileImg} />
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
+      </InfoTestComp>
       <div className="Home_innerBox">
         <UserInfoCom1></UserInfoCom1>
         <MarginStyle1 />
@@ -54,4 +95,12 @@ const MarginStyle1 = styled.div`
 `;
 const MarginStyle2 = styled.div`
   margin-top: 50px;
+`;
+
+const InfoTestComp = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: white;
+  background-color: black;
 `;
